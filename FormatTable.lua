e2d61bedbb2b79a781e6ed1958f289f2deb6973c
MIN <1154177150@qq.com>
2019-08-01 19:50:40 +0800

    Create FormatTable.lua
    doSomething

---
+++ FormatTable.lua
@@ -0,0 +1,29 @@
+function GuildPanel.FormatValue(val)
+    if type(val) == "string" then
+        return string.format("%q", val)
+    end
+    return tostring(val)
+end
+
+function GuildPanel.FormatTable(t, tabcount)
+    tabcount = tabcount or 0
+    -- if tabcount > 5 then
+    --     --防止栈溢出
+    --     return "<table too deep>"..tostring(t)
+    -- end
+    local str = ""
+    if type(t) == "table" then
+        for k, v in pairs(t) do
+            local tab = string.rep("\t", tabcount)
+            if type(v) == "table" then
+                str = str..tab..string.format("[%s] = {", this.FormatValue(k))..'\n'
+                str = str..this.FormatTable(v, tabcount + 1)..tab..'}\n'
+            else
+                str = str..tab..string.format("[%s] = %s", this.FormatValue(k), this.FormatValue(v))..',\n'
+            end
+        end
+    else
+        str = str..tostring(t)..'\n'
+    end
+    return str
+end
